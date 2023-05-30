20e2620aaffe366721d652faa6bfe511fb02a2b4
ahmedtaeha <74939755+ahmedtaeha@users.noreply.github.com>
2023-05-30 15:49:57 -0700

    Uploading up to lab 3

---
+++ CSC 60/Lab2/lab2.c
@@ -0,0 +1,17 @@
+/* Ahmed Taeha */
+/* Lab 2*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+{
+
+    printf("\nLab 2. \n\n");
+    printf("Hello World!.\n");
+    printf("Hi, Ahmed Taeha.\n\n");
+    printf("Nothing is impossible - Audrey Hepburn.\n\n");
+
+    return EXIT_SUCCESS;
+}
